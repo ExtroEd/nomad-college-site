f792c5fd331fe5688e63a1e346852c0f1a91bad1
unknown <extroed@gmail.com>
2024-04-08 15:31:53 +0600

    22:26 второе видео

---
+++ src/pages/Students/Students.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+
+const Students = () => {
+    // const [data, setData] = useState([])
+    // const getStudents = () => {
+    //     axios("")
+    // }
+    return (
+        <section className={"students"}>
+            <div className="container">
+                <div className="students__row">
+
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Students;
